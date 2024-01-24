2f870d66223440c30e29b39b5695a23ff92bbab3
Trent Small <sixstring982@gmail.com>
2024-01-24 16:32:40 -0700

    Update templates/ocaml/dune-project for easier use

--- templates/ocaml/dune-project/flake.nix
+++ templates/ocaml/dune-project/flake.nix
@@ -1,31 +1,26 @@
-# OCaml/Dune project template
-#
-# Once you init this flake, you'll also need to:
-# - Change the description
-# - Replace all "project-name-change-me" instances with your project name
-#
-# For development, simply run `nix develop`.
 {
-  description = "Project Description";
+  description = "Description of my project";
 
   inputs = {
-    # Convenience functions for writing flakes
     flake-utils.url = "github:numtide/flake-utils";
-    # Precisely filter files copied to the nix store
     nix-filter.url = "github:numtide/nix-filter";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nix-filter, flake-utils, nixpkgs }:
+  outputs = { self, nixpkgs, flake-utils, nix-filter }:
+    let
+      project = {
+        name = "My OCaml project";
+        version = "0.0.1";
+
+        ocamlVersion = "ocamlPackages_5_1";
+      };
+    in
     flake-utils.lib.eachDefaultSystem (system:
       let
-        # Legacy packages that have not been converted to flakes
         legacyPackages = nixpkgs.legacyPackages.${system};
-        # OCaml packages available on nixpkgs
-        ocamlPackages = legacyPackages.ocaml-ng.ocamlPackages_5_1;
-        # Library functions from nixpkgs
-        lib = legacyPackages.lib;
+        ocamlPackages = legacyPackages.ocaml-ng.${project.ocamlVersion};
 
-        # Filtered sources (prevents unecessary rebuilds)
         sources = {
           ocaml = nix-filter.lib {
             root = ./.;
@@ -46,66 +41,39 @@
           };
         };
       in
-      {
-        # Exposed packages that can be built or run with `nix build` or
-        # `nix run` respectively:
-        #
-        #     $ nix build .#<name>
-        #     $ nix run .#<name> -- <args?>
-        #
-        packages = {
-          # The package that will be built or run by default. For example:
-          #
-          #     $ nix build
-          #     $ nix run -- <args?>
-          #
-          default = self.packages.${system}.project-name-change-me;
 
-          project-name-change-me = ocamlPackages.buildDunePackage {
-            pname = "project-name-change-me";
-            version = "0.0.1";
+      {
+        packages = {
+          default = self.packages.${system}.${project.name};
+
+          ${project.name} = ocamlPackages.buildDunePackage {
+            pname = project.name;
+            version = project.version;
             duneVersion = "3";
             src = sources.ocaml;
 
             buildInputs = [
-              # Ocaml package dependencies needed to build go here, e.g.:
-              # ocamlPackages.opium
+              # Add any inputs used to build your OCaml project here
             ];
 
             strictDeps = true;
 
-            preBuild = ''
-              dune build project-name-change-me.opam
-            '';
+            preBuild = "dune build ${project.name}.opam";
           };
         };
 
-        # Development shells
-        #
-        #    $ nix develop .#<name>
-        #    $ nix develop .#<name> --command dune build @test
-        #
-        # [Direnv](https://direnv.net/) is recommended for automatically loading
-        # development environments in your shell. For example:
-        #
-        #    $ echo "use flake" > .envrc && direnv allow
-        #    $ dune build @test
-        #
         devShells = {
           default = legacyPackages.mkShell {
-            # Development tools
             packages = [
               legacyPackages.fswatch
               legacyPackages.ocamlformat
               ocamlPackages.ocaml-lsp
             ];
 
-            # Tools from packages
             inputsFrom = [
-              self.packages.${system}.project-name-change-me
+              self.packages.${system}.${project.name}
             ];
           };
         };
       });
 }
-
