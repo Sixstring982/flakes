6d67e5d3fe27c191d11cdcf42a25b957b7d913ff
Trent Small <sixstring982@gmail.com>
2024-01-27 20:02:31 -0700

    Add templats/haskell/rio -- a RIO template

--- flake.nix
+++ flake.nix
@@ -1,15 +1,22 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs }:
     {
-      templates.ocaml-dune-project = {
-        description =
-          "Simple OCaml dune project template";
-        path = ./templates/ocaml/dune-project;
+      templates = {
+        ocaml-dune-project = {
+          description =
+            "Simple OCaml dune project template";
+          path = ./templates/ocaml/dune-project;
+        };
+
+        haskell-rio = {
+          description =
+            "Haskell project template using the RIO library";
+          path = ./templates/haskell/rio;
+        };
       };
     };
 }
---
+++ templates/haskell/rio/README.md
@@ -0,0 +1,24 @@
+# my-project
+
+## Execute
+
+* Run `stack exec -- my-project-exe` to see "We're inside the application!"
+* With `stack exec -- my-project-exe --verbose` you will see the same message, with more logging.
+
+## Run tests
+
+`stack test`
+
+{-# START_FILE CHANGELOG.md #-}
+# Changelog for `{{ name }}`
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to the
+[Haskell Package Versioning Policy](https://pvp.haskell.org/).
+
+## Unreleased
+
+## 0.1.0.0 - YYYY-MM-DD
+
